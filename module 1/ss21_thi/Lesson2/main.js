49cced0ea4dd8468dfbc2b5dac5a8a4d15c1cd5d
nguyenthanhlinh22 <nguyenthanhlinh22@gmail.com>
2024-04-22 20:02:28 +0700

    commit bai 2

--- module 1/ss21_thi/Lesson2/main.js
+++ module 1/ss21_thi/Lesson2/main.js
@@ -1,10 +1,16 @@
 function  insertNumberToArray(){
-    let arr = [1, 2, 3, 4, 5, 4, 3, 2, 1];
-    let number = 10;
+    let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    let number = 111;
     let index = 5;
-    arr.splice(index, 0, number);
-    for (let i = 0; i < arr.length; i++){
-        console.log(arr[i]);
+    
+  
+    if(index < 0 || index >= arr.length){
+        alert("Can't insert number to array");
+    }else{
+        arr.splice(index, 0, number);
+        for (let i = 0; i < arr.length; i++){
+            console.log(arr[i]);
+        }
     }
     
 }
